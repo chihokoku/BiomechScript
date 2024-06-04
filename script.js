541e773070481c6af5eeab4fdf9523e0e1493be0
maccha <chihokoku8060@gmail.com>
2024-06-04 12:27:57 +0900

    add:objLoader feature

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  console.log("Hello World!");
-
   const canvas = document.querySelector("#c");
 
   // レンダラーの設定
@@ -15,40 +13,60 @@ function init() {
   const fov = 75;
   const aspect = 2; // the canvas default
   const near = 0.1;
-  const far = 5;
+  const far = 200;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 2;
+  camera.position.z = 100;
 
   // シーンの設定
   const scene = new THREE.Scene();
 
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  // カメラコントロールができるようにする(オービットコントロールを作成)
+  const controls = new THREE.OrbitControls(camera, canvas);
+  controls.enableDamping = true; // 慣性の有効化
+  controls.dampingFactor = 0.25;
 
-  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+  // ファイル入力要素の取得
+  const fileInput = document.getElementById("fileInput");
 
-  const cube = new THREE.Mesh(geometry, material);
+  // ファイルが選択されたときの処理
+  fileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const contents = e.target.result;
 
-  scene.add(cube);
-  // renderer.render(scene, camera);
+        // OBJLoaderで読み込み
+        const objLoader = new THREE.OBJLoader();
+        const object = objLoader.parse(contents);
+        object.position.set(0, 0, 0);
+        scene.add(object);
+      };
+      reader.readAsText(file);
+    }
+  });
 
-  // ライトを追加
-  const color = 0xffffff;
-  const intensity = 3;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
+  // 環境光源を作成
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
 
+  // 平行光源を作成
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.intensity = 1;
+  directionalLight.position.set(1, 10, 1);
+  scene.add(directionalLight);
+
+  // 座標軸の表示
+  // new THREE.AxesHelper(軸の長さ);
+  const axis = new THREE.AxesHelper(100);
+  scene.add(axis);
+
+  // 画面をレンダリング(アニメーション)
   tick();
 
   function tick() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
-
+    controls.update();
     requestAnimationFrame(tick);
   }
 }
